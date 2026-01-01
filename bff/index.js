f6b5e4c8acf29642f4da0fec4ddbbf8f2262a5f8
Alex <alexprogressdeveloper@gmail.com>
2026-01-01 19:45:21 +0300

    prod mode

--- bff/index.js
+++ bff/index.js
@@ -5,8 +5,16 @@ const path = require("path");
 const app = express();
 const PORT = 3500;
 
+const isProd = process.env.NODE_ENV === "production";
 app.use(express.json());
 
+
+if (isProd) {
+  // 👉 PROD: раздаём dist
+  app.use(
+    express.static(path.join(__dirname, "../frontend/dist"))
+  );
+}
 
 app.get("/api/message", async (req, res) => {
   const response = await fetch("http://localhost:3501/message");
@@ -24,6 +32,15 @@ app.post("/api/message", async (req, res) => {
   const data = await response.json();
   res.json(data);
 });
+
+
+if (isProd) {
+  app.use((req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend/dist/index.html")
+    );
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`BFF running on http://localhost:${PORT}`);
